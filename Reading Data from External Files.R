022264defca0c92cf6c57a617dd4128a1b9310ba
waraba <139711993+Alphabf@users.noreply.github.com>
2024-07-12 18:45:25 -0400

    Update Reading Data from External Files.R

--- Reading Data from External Files.R
+++ Reading Data from External Files.R
@@ -8,5 +8,3 @@ library(tidyverse)
 ## the code to read the data and view/print the data
 
 ## Example 1 import text
-
-mydata <- read.table("C:/Users/traor/Desktop/R Playground/Learning SAS to R from doc/SAS_Learning_SAS_by_Example_A_Programmers_Guide/Learning/mydata.txt", quote="\"", comment.char="", stringsAsFactors=TRUE)
